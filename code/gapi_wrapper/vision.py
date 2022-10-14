33cf92dcd8b9387c8d87a70c11114c0fc4580f4f
rukayaj@gmail.com <rukayaj@gmail.com>
2022-10-14 15:32:16 +0200

    Small docker compose change and fix logging message

--- code/gapi_wrapper/vision.py
+++ code/gapi_wrapper/vision.py
@@ -5,7 +5,7 @@ def gv_ocr(content):
     gvclient = vision.ImageAnnotatorClient()
     image = vision.Image(content=content)
 
-    logging.info(f'Attempting to ocr via Google Cloud Vision - {image}')
+    logging.info(f'Attempting to ocr via Google Cloud Vision...')
     response = gvclient.document_text_detection(image=image)
     if response.error.code:
         raise Exception(f"Error from Google Cloud Vision - {response.error}")
